4d13938ef1ced6ce737e6563641505a5153fd930
pavel <pavelrussko@gmail.com>
2024-12-09 12:28:56 +0200

    add AVL_tree.h

---
+++ AVL_tree.h
@@ -0,0 +1,27 @@
+#include "wet1util.h"
+template <class T>
+
+struct TreeNode{
+    T* data;
+    TreeNode<T>* left;
+    TreeNode<T>* right;
+    TreeNode<T>* parent;
+    int height;
+};
+
+template<class T>
+class AVL_Tree
+{
+private:
+    TreeNode<T>* root;
+public:
+    StatusType insert(TreeNode<T>*);
+    TreeNode<T>* search(TreeNode<T>*);
+    StatusType removal(TreeNode<T>*);
+    void inorder(TreeNode<T>*);
+    void LL_rotation();
+    void LR_rotation();
+    void RR_rotation();
+    void Rl_rotation();
+    int height_difference(TreeNode<T>*);
+};
