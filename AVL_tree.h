632bebe9a67cc0f88336a4dbb64c17e031d550d3
Ofek <ofek5202@gmail.com>
2024-12-10 12:10:11 +0200

    Add templates for function in AVL_tree.cpp

--- AVL_tree.h
+++ AVL_tree.h
@@ -1,27 +1,28 @@
 #include "wet1util.h"
-template <class T>
 
-struct TreeNode{
-    T* data;
-    TreeNode<T>* left;
-    TreeNode<T>* right;
-    TreeNode<T>* parent;
+template<class T>
+
+struct TreeNode {
+    T *data;
+    TreeNode<T> *left;
+    TreeNode<T> *right;
+    TreeNode<T> *parent;
     int height;
 };
 
 template<class T>
-class AVL_Tree
-{
+class AVL_Tree {
 private:
-    TreeNode<T>* root;
+    TreeNode<T> *root;
 public:
-    StatusType insert(TreeNode<T>*);
-    TreeNode<T>* search(TreeNode<T>*);
-    StatusType removal(TreeNode<T>*);
-    void inorder(TreeNode<T>*);
+    AVL_Tree();
+    StatusType insert(TreeNode<T> *);
+    TreeNode<T> *search(TreeNode<T> *);
+    StatusType removal(TreeNode<T> *);
+    void inorder(TreeNode<T> *);
     void LL_rotation();
     void LR_rotation();
     void RR_rotation();
     void Rl_rotation();
-    int height_difference(TreeNode<T>*);
+    int height_difference(TreeNode<T> *);
 };
